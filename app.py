9a63b20322f90ed7c65ccdcd3613946f661eb2f7
PcpSeven <146136929+PcpSeven@users.noreply.github.com>
2023-09-27 13:40:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,40 @@ import sqlite3
 def criarTable():
     banco  = sqlite3.connect('ControleDeRotasSeven.bd')
     cursor = banco.cursor()
-    cursor.execute('CREATE TABLE FIORINO (ID INTEGER, FUNCIONARIO text, DATAHORA datetime , SAIDACHEGADA text, ROTA text, KM text, OBS text)')
-    #cursor.execute('INSERT INTO FIORINO VALUES ('+GOOGLE+')')
+    cursor.execute('CREATE TABLE carros (FUNCIONARIO text,CARRO text, DATAHORA datetime , SAIDACHEGADA text, ROTA text, KM text, LOCALIZACAO text)')
     banco.commit()
     cursor.close()
 
-def teste():
-    GOOGLE = 'NERI'
+def Inserir(content):
     banco  = sqlite3.connect('ControleDeRotasSeven.bd')
     cursor = banco.cursor()
-    cursor.execute('INSERT INTO FIORINO VALUES ()')
+    lista = [
+        content['motorista'],content['carro'],content['dataHora'],content['chegadaSaida'],content['rota'],content['km'],content['loc']
+    ]
+    cursor.execute('INSERT INTO carros VALUES(?,?,?,?,?,?,?)',lista)
     banco.commit()
     cursor.close()
+
+
+def getTable():
+    lista = []
+    try:
+        connection = sqlite3.connect('ControleDeRotasSeven.bd')
+        cursor = connection.cursor()
+        sqlite_select_query = """SELECT * FROM carros"""
+        print(sqlite_select_query)
+        cursor.execute(sqlite_select_query)
+        obj = cursor.fetchall()
+        for i in obj:
+            lista.append({'FUNCIONARIO':i[0], "CARRO": i[1],"DATAHORA":i[3], "SAIDACHEGADA":i[2],"ROTA":i[4],"KM":i[5],"LOCALIZACAO":i[6]})
+        cursor.close()
+        return lista
+    except sqlite3.Error as error:
+        print("Failed to read data from table", error)
+    finally:
+        if connection:
+            connection.close()
+            print("The Sqlite connection is closed")
 
 
 def DropTable(tabela,limparlista):
@@ -35,12 +57,25 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
-@app.route("/",methods = ['POST', 'GET'])
+#       Adiciona item na tabela
+@app.route("/",methods = ['POST'])
 def responder():
     content = request.json
+    Inserir(content)
     print(content)
     return '200'
+
+#       Criar Tabela
+@app.route("/criar",methods = ['GET'])
+def criar():
+    criarTable()
+    return '200'
+
+#       Retorno a tabela
+@app.route("/buscar",methods = ['GET'])
+def buscar():
+    lista = getTable()
+    return jsonify(lista)
 
 
 if __name__ == '__main__':
